5e6051f744c6b4b1ceefcd1551ad43bb4eaa3be0
yashicamanohar <yashicamanoharofficial@gmail.com>
2025-05-14 15:18:01 +0530

    Add files via upload

---
+++ vowel.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    char word;
+    scanf("%c",&word);
+    if((word=='a'||word=='e'||word=='i'||word=='o'||word=='u'))
+    printf("it is the vowel");
+    else{
+        printf("it is not the vowel");
+    }
+    
+}
